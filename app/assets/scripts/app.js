2a9cf5d506b95d29f384f9f762bebb0ec08eb33f
Roberto Panzeri <robertopanzeri@rocketmail.com>
2019-03-05 15:49:27 +0100

    creato menu scroll desktop/mobile

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader();
--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,17 +3,25 @@ class MobileMenu {
        // selecting from the DOM
        this.menuIcon = document.getElementsByClassName('site-header__menu-icon');
        this.primaryNav = document.getElementsByClassName('site-header__primary-nav');
+       this.primaryNavLink = this.primaryNav[0].getElementsByTagName('a');
+       this.primaryNavLinkArr = Array.from(this.primaryNavLink);
+       this.menuModal = document.getElementsByClassName('site-header__menu-modal');
        // activating events handler
        this.events();
     }
 
     events() { //events handling
+        var that = this;
         this.menuIcon[0].onclick = this.toggleMenu.bind(this);
+        this.primaryNavLinkArr.forEach(function(currentValue){
+            currentValue.onclick = that.toggleMenu.bind(that);
+        });
     }
 
     toggleMenu () { // defining functionality
         this.primaryNav[0].classList.toggle('site-header__primary-nav--is-visible');
         this.menuIcon[0].classList.toggle('site-header__menu-icon--close-x');
+        this.menuModal[0].classList.toggle('site-header__menu-modal--is-visible');
     }
 }
 
